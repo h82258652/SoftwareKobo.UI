deecb2ac8165ab0560166ade37cbd2dc6665b072
h82258652 <h82258652@hotmail.com>
2015-11-29 00:00:03 +0800

    update

---
+++ SoftwareKobo.UI/SoftwareKobo.UI/Xaml/Controls/Button.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Automation.Peers;
+using Windows.UI.Xaml.Automation.Provider;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Documents;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+
+// The Templated Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234235
+
+namespace SoftwareKobo.UI.Xaml.Controls
+{
+    public class Button : Control
+    {
+        public Button()
+        {
+            this.DefaultStyleKey = typeof(Button);
+        }
+
+        public void PerformClick()
+        {
+            var peer = FrameworkElementAutomationPeer.FromElement(this) ?? FrameworkElementAutomationPeer.CreatePeerForElement(this);
+            var provider = (IInvokeProvider)peer.GetPattern(PatternInterface.Invoke);
+            provider.Invoke();
+        }
+    }
+}
---
+++ SoftwareKobo.UI/SoftwareKobo.UI/Xaml/Controls/Frame.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Animation;
+
+namespace SoftwareKobo.UI.Xaml.Controls
+{
+    public class Frame : Windows.UI.Xaml.Controls.Frame
+    {
+        public Frame() : base()
+        {
+        }
+
+        public bool Navigate<TPage>() where TPage : Page
+        {
+            return Navigate(typeof(TPage));
+        }
+
+        public bool Navigate<TPage>(object parameter) where TPage : Page
+        {
+            return Navigate(typeof(TPage), parameter);
+        }
+
+        public bool Navigate<TPage>(object parameter, NavigationTransitionInfo infoOverride)
+        {
+            return Navigate(typeof(TPage), parameter, infoOverride);
+        }
+    }
+}
